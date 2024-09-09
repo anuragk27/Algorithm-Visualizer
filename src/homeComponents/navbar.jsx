d75cdfc55358ccf8f4d8ffaed8e88b2cbb2a487c
Anurag kumar <95006508+anuragk27@users.noreply.github.com>
2024-09-09 12:53:27 +0530

    Update navbar.jsx

--- src/homeComponents/navbar.jsx
+++ src/homeComponents/navbar.jsx
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-                <a className="navbar-brand" style={{fontFamily:'serif'}} href="#">Algorithm Visualizer</a>
+                <a className="navbar-brand" style={{fontFamily:'serif'}} href="#">Algorithm Visualize</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
